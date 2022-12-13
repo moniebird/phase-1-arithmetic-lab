769fb77a52a8df2d107f305f38d245b01e5b9560
moniebird <moniebird@mac.com>
2022-12-13 18:43:48 -0500

    commit tree c8969d8414bf5eede8b5a83a0478fbef98ce324c

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() > 0);
+
+let num3 = 12
+let num4 = 3
+let mod = num3 / num4;
+
+let max = Math.max(5, 10, 15, 20);
